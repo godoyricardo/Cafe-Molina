f2fdea0c8f0df0b0a43d3bf36f2721bdc8bb7f47
EmiTorcal <emitorcal@hotmail.com>
2022-10-16 22:30:13 -0300

    Navegador fijo cuando < 910px

--- recursos/js/header.js
+++ recursos/js/header.js
@@ -2,16 +2,32 @@
 
 let encabezado = document.getElementById('encabezado');
 let Bt_arriba = document.getElementById('Boton_arriba');
+
+
 window.addEventListener('scroll', function () {
     let posicion = this.scrollY;
-    console.log(posicion);
-    if (posicion > 0) {
+    let windowWidth = window.innerWidth;
+    if (posicion > 0 && windowWidth > 910) {
         encabezado.classList.remove('header')
         encabezado.classList.add('header2')
 
         Bt_arriba.style.display = 'inline-block';
 
-    } else {encabezado.classList.remove('header2')
-    encabezado.classList.add('header')
-    Bt_arriba.style.display = 'none';}
+    } else {
+        encabezado.classList.remove('header2')
+        encabezado.classList.add('header')
+        Bt_arriba.style.display = 'none';
+    }
+
+    if (posicion > 0 && windowWidth < 910) {
+        encabezado.classList.remove('header')
+        encabezado.classList.add('header3')
+
+        Bt_arriba.style.display = 'inline-block';
+
+    } else {
+        encabezado.classList.remove('header3')
+        encabezado.classList.add('header')
+        Bt_arriba.style.display = 'none';
+    }
 })
